236a698223fc697572fd78508f6961b0c353be04
shada <shadabfaizal7@gmail.com>
2020-05-19 13:30:26 +0530

    bottom navigation and app skeleton implemented

---
+++ app/src/main/java/com/faizal/shadab/weatherforecasetmvvm/ui/settings/SettingsFragment.kt
@@ -0,0 +1,6 @@
+package com.faizal.shadab.weatherforecasetmvvm.ui.settings
+
+import androidx.fragment.app.Fragment
+
+class SettingsFragment : Fragment() {
+}
---
+++ app/src/main/java/com/faizal/shadab/weatherforecasetmvvm/ui/weather/current/CurrentWeatherViewModel.kt
@@ -0,0 +1,7 @@
+package com.faizal.shadab.weatherforecasetmvvm.ui.weather.current
+
+import androidx.lifecycle.ViewModel
+
+class CurrentWeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
