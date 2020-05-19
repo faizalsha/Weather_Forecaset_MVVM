ca8f1ab9e2023f9f6d70d19d2a32b1325f19643e
shada <shadabfaizal7@gmail.com>
2020-05-19 18:54:13 +0530

    getting json response object from api

--- app/src/main/java/com/faizal/shadab/weatherforecasetmvvm/ui/weather/current/CurrentWeatherFragment.kt
+++ app/src/main/java/com/faizal/shadab/weatherforecasetmvvm/ui/weather/current/CurrentWeatherFragment.kt
@@ -8,6 +8,11 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.faizal.shadab.weatherforecasetmvvm.R
+import com.faizal.shadab.weatherforecasetmvvm.data.ApixuWeatherApiService
+import kotlinx.android.synthetic.main.current_weather_fragment.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class CurrentWeatherFragment : Fragment() {
 
@@ -29,6 +34,15 @@ class CurrentWeatherFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(CurrentWeatherViewModel::class.java)
         // TODO: Use the ViewModel
+
+        //TODO: Remove this code (unlike MVVM architecture)
+        val apiService = ApixuWeatherApiService()
+
+        GlobalScope.launch(Dispatchers.Main) {
+            val currentWeatherResponse = apiService.getCurrentWeather("London").await()
+            tempTextView.text = currentWeatherResponse.toString()
+        }
+
     }
 
 }
