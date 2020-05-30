3840e7dd6e36845885e69646764b131de491dbd7
shada <shadabfaizal7@gmail.com>
2020-05-30 14:28:55 +0530

    entity changed

--- app/src/main/java/com/faizal/shadab/weatherforecasetmvvm/data/db/entity/CurrentWeatherEntry.kt
+++ app/src/main/java/com/faizal/shadab/weatherforecasetmvvm/data/db/entity/CurrentWeatherEntry.kt
@@ -9,22 +9,20 @@ const val CURRENT_WEATHER_ID = 0
 
 @Entity(tableName = "current_weather")
 data class CurrentWeatherEntry(
-    val feelslike: Int,
+    val feelslike: Double,
     @SerializedName("is_day")
     val isDay: String,
     @SerializedName("observation_time")
     val observationTime: String,
-    val precip: Int,
-    val temperature: Int,
+    val precip: Double,
+    val temperature: Double,
     @SerializedName("uv_index")
-    val uvIndex: Int,
-    val visibility: Int,
-    @SerializedName("weather_code")
-    val weatherCode: Int,
+    val uvIndex: Double,
+    val visibility: Double,
     @SerializedName("wind_dir")
     val windDir: String,
     @SerializedName("wind_speed")
-    val windSpeed: Int
+    val windSpeed: Double
 ) {
     @PrimaryKey(autoGenerate = false)
     var id :  Int = CURRENT_WEATHER_ID
